19a45cf5ff8c4ee2f4c9cae035e6533303e09cae
Rafał "Huncwot" Michalski <rafal.michalski@redefine.pl>
2018-10-29 10:24:44 +0100

    init

---
+++ ext.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace huncwot\moderatortools;
+
+class ext extends \phpbb\extension\base
+{
+
+    public function is_enableable()
+    {
+        return phpbb_version_compare(PHPBB_VERSION, '3.2.0', '>=');
+    }
+}
